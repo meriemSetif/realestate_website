47e396f50b190e90468b7a50d497f1f929b1a5ab
yahiaAlg <yahialinus21@gmail.com>
2024-02-10 11:44:34 +0100

    'another one'

--- realestate_proj/asgi.py
+++ realestate_proj/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for academy_courses project.
+ASGI config for realestate_proj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academy_courses.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realestate_proj.settings')
 
 application = get_asgi_application()
--- realestate_proj/wsgi.py
+++ realestate_proj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for academy_courses project.
+WSGI config for realestate_proj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academy_courses.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realestate_proj.settings')
 
 application = get_wsgi_application()
